3455f2b78f81a1bda802cdc4d88b25f7f379d514
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-08-15 20:22:13 -0300

    deploy

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,8 +2,7 @@ import { Request, Response } from "express"
 
 import authService from "../services/authService.js"
 
-async function signUp(req: Request, res: Response){ 
-    console.log("ta chegando requisição")       
+async function signUp(req: Request, res: Response){            
     const user = req.body    
     await authService.insertUser(user)
     res.sendStatus(200)
@@ -12,8 +11,7 @@ async function signUp(req: Request, res: Response){
 async function signIn(req: Request, res: Response) {    
     const user = req.body
     const token = await authService.signIn(user)
-    const response = {token : token.token, userId: token.userId}    
-    res.send(response).status(200)
+    res.sendStatus(200)
 }
 
 export default { 
