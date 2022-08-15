42f9e7bd6028cebac7cebea1f488eaaba90cd7c6
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-08-15 19:46:06 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ app.use(json())
 app.use(router)
 app.use(errorHandlerMiddleware)
 
-app.listen(process.env.PORT, () =>{
+app.listen(process.env.PORT || 4000, () =>{
     console.log("Server listening on port " + process.env.PORT)
 })
