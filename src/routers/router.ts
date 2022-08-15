66ea830571c3001e8b4cd452ab889a886f1fe149
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-08-15 19:41:21 -0300

    deploy

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -3,8 +3,6 @@ import authRouter from "./authRouter.js"
 import buyNumberRouter from "./buyNumberRouter.js"
 import raffleRouter from "./raffleRouter.js"
 import reserveRouter from "./reserveNumber.js"
-import testsRouter from "./testRouter.js"
-
 
 const router = Router()
 
@@ -12,6 +10,6 @@ router.use(authRouter)
 router.use(raffleRouter)
 router.use(buyNumberRouter)
 router.use(reserveRouter)
-router.use(testsRouter)
+
 
 export default router
