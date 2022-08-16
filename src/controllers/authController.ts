a2b5ba1980666c13c14539d8b544fc32404185dd
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-08-16 16:15:03 -0300

    deploy

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -8,7 +8,8 @@ async function signUp(req: Request, res: Response){
     res.sendStatus(200)
 }
 
-async function signIn(req: Request, res: Response) {    
+async function signIn(req: Request, res: Response) {  
+      
     const user = req.body
     const token = await authService.signIn(user)
     res.send({ token : token.token, userId: token.userId }).status(200)
